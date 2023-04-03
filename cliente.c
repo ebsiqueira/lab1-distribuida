7751a7b8da944b76eec08645f56d9157dda9af02
Eduardo Borges Siqueira <eduardo@MacBook-Air-de-Eduardo.local>
2023-04-03 19:52:37 -0300

    versao estruturada

---
+++ cliente.c
@@ -0,0 +1,81 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int sockfd;
+    int len;
+    struct sockaddr_in address;
+    int result;
+    char op[3], key[5], value[10], cmd[21];
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    // address.sin_addr.s_addr = inet_addr("");
+    address.sin_port = htons(9734);
+
+    len = sizeof(address);
+    result = connect(sockfd, (struct sockaddr *)&address, len);
+    if (result == -1)
+    {
+        perror("oops: client1");
+        exit(1);
+    }
+    while (1)
+    {
+        printf("type operation (put or get): ");
+        scanf("%s", &op);
+        if (strcmp(op, "put") == 0)
+        {
+            printf("type key: ");
+            scanf("%s", &key);
+            printf("type value: ");
+            scanf("%s", &value);
+            strcpy(cmd, op);
+            strcat(cmd, "(");
+            strcat(cmd, key);
+            strcat(cmd, ",");
+            strcat(cmd, value);
+            strcat(cmd, ")");
+            //printf("%s", &cmd);
+            write(sockfd, &cmd, sizeof(cmd));
+        }
+        else
+        {
+            if (strcmp(op, "get") == 0)
+            {
+                printf("type key: ");
+                scanf("%s", &key);
+                strcpy(cmd, op);
+                strcat(cmd, "(");
+                strcat(cmd, key);
+                strcat(cmd, ")");
+                write(sockfd, &cmd, sizeof(cmd));
+                read(sockfd, &value, sizeof(value));
+                printf("%s", &value);
+            }
+            else
+            {
+                if (strcmp(op, "quit") == 0)
+                {
+                    char quit = 'q';
+                    write(sockfd, &quit, 1);
+                    break;
+                }
+                else
+                {
+                    printf("invalid operation\n");
+                }
+            }
+        }
+    }
+    close(sockfd);
+    exit(0);
+}
---
+++ servidor.c
@@ -0,0 +1,99 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+#define key_size 5
+#define value_size 10
+#define max_clients 2
+
+pthread_mutex_t store_lock;
+char store[key_size];
+
+void *user(void *arg);
+
+int main()
+{
+    int server_sockfd, client_sockfd;
+    int server_len, client_len;
+    struct sockaddr_in server_address;
+    struct sockaddr_in client_address;
+    pthread_t tid[max_clients + 5];
+    pthread_mutex_init(&store_lock, NULL);
+
+    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(9734);
+    server_len = sizeof(server_address);
+    bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
+    listen(server_sockfd, max_clients);
+    int i = 0;
+    while (1)
+    {
+        printf("server waiting\n");
+        client_len = sizeof(client_address);
+        client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &client_len);
+        if (pthread_create(&tid[i++], NULL, user, &client_sockfd) != 0)
+        {
+            printf("failed to create thread\n");
+        }
+        else
+        {
+            printf("thread %i created\n", i);
+        }
+        if (i > max_clients)
+        {
+            i = 0;
+            while (i < max_clients)
+            {
+                pthread_join(tid[i++], NULL);
+                printf("thread %i finished\n", i);
+            }
+            i = 0;
+        }
+    }
+}
+void *user(void *arg)
+{
+    int client_sockfd = *((int *)arg);
+    char cmd[21];
+    char rtn[value_size];
+    char key[key_size];
+    char value[value_size];
+    while (1)
+    {
+        read(client_sockfd, &cmd, sizeof(cmd));
+        if (cmd[0] == 'p' && cmd[1] == 'u' && cmd[2] == 't')
+        {
+            printf("%s", &cmd);
+            char *token = strtok(cmd, ",");
+            for(int i = 4; i < sizeof(token); i++){
+                key[i-4] = token[i];
+            }
+            token = strtok(NULL, "");
+            for(int i = 0; i < sizeof(token)-1; i++){
+                value[i] = token[i];
+            }
+            // pthread_mutex_lock(&store_lock);
+            // pthread_mutex_unlock(&store_lock);
+        }
+        if (cmd[0] == 'g' && cmd[1] == 'e' && cmd[2] == 't')
+        {
+            // pthread_mutex_lock(&store_lock);
+            // pthread_mutex_unlock(&store_lock);
+            write(client_sockfd, &rtn, sizeof(rtn));
+        }
+        else
+        {
+            close(client_sockfd);
+            break;
+        }
+    }
+    pthread_exit(NULL);
+}
